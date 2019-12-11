7ebc504ceedbeb58343623ceccd5d1a167b379de
pure1017 <47238756+pure1017@users.noreply.github.com>
2019-12-11 09:57:56 -0500

    Update wsgi.py

--- webapp_django/wsgi.py
+++ webapp_django/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp_django.settings')
 
 application = get_wsgi_application()
